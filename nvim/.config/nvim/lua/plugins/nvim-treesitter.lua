9f1b9428d86ec5787c38bbc0aa99158c180bc9e9
Luis Narquel <lmnarqueldev@gmail.com>
2026-01-17 14:59:40 +0000

    force nvim-treesitter to master branch to avoid new breaking version

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,27 +1,41 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-        local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-            ensure_installed = {
-                "c", "java", "lua", "vim", "vimdoc", "query", "javascript", "python", "html", "markdown", "markdown_inline", "sql", "json", "dockerfile"
-            },
-            auto_install = true,
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },  
+		configs.setup({
+			ensure_installed = {
+				"c",
+				"java",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"javascript",
+				"python",
+				"html",
+				"markdown",
+				"markdown_inline",
+				"sql",
+				"json",
+				"dockerfile",
+			},
+			auto_install = true,
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
 
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<Enter>", -- set to `false` to disable one of the mappings
-                    node_incremental = "<Enter>",
-                    scope_incremental = false,
-                    node_decremental = "<Backspace>",
-                },
-            },
-        })
-    end
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<Enter>", -- set to `false` to disable one of the mappings
+					node_incremental = "<Enter>",
+					scope_incremental = false,
+					node_decremental = "<Backspace>",
+				},
+			},
+		})
+	end,
 }
